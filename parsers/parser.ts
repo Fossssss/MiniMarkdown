86816fbdda98b395db051667bb30216cfc5bdbbc
qazwdz <13767423094@163.com>
2025-02-20 11:35:08 +0800

    Update parser.ts

--- parsers/parser.ts
+++ parsers/parser.ts
@@ -5,8 +5,9 @@ import { parseParagraphs } from './paragraphParser'
 import { createListParser } from './listParser'
 import { parseQuotes } from 'quoteParser.ts'
 import { parseHorizontalRules } from 'horizontalRuleParser.ts'
-import { parseBold } from 'bold italic 
-
+import { parseBold } from 'bold italic strikethrough Parser.ts'
+import { parseItalic } from 'bold italic strikethrough Parser.ts'
+import { parseStrikethrough } form 'bold italic strikethrough Parser.ts'
 
 export function parseMarkdown(str: string) {
   const tokens = []
