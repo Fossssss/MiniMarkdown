8ffda8b06f60a701b0e603160e913467c1e8e218
qazwdz <13767423094@163.com>
2025-02-24 23:09:41 +0800

    Update horizontalRuleParser.ts

--- parsers/horizontalRuleParser.ts
+++ parsers/horizontalRuleParser.ts
@@ -11,6 +11,7 @@ function createHrToken() {
 
 export function parseHorizontalRules(str: string) {
   const hrRegex = /^\s*([*-]{3,})\s*$/; // 确保至少三个连续的符号
+  //——————或者***有效，而——*——类似的无效
 
   if (hrRegex.test(str)) {
     return [createHrToken()];
